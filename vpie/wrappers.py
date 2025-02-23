d9bc2c2b3ee491150c4e9c29f03736f2e93cfb7b
Ted Johnson <ted.johnson@unlv.edu>
2025-02-23 08:18:32 -0800

    Add `get_vpie` function that combines many of the rust funcs

--- vpie/wrappers.py
+++ vpie/wrappers.py
@@ -4,8 +4,8 @@ Python wrappers for Rust functions.
 Just so language servers can find them.
 """
 
+from typing import Set, Tuple
 import numpy as np
-from typing import Set
 
 from . import _vpie_rs
 
@@ -92,3 +92,37 @@ def get_reconstruction(
         The reconstructed observation.
     """
     return _vpie_rs.get_reconstruction(flux, coeffs, s)
+
+def get_vpie(
+    f_org: np.ndarray,
+    f_err: np.ndarray,
+    cutoff_index: int,
+    use_mean_error: bool
+) -> Tuple[Set[int], np.ndarray, np.ndarray]:
+    """
+    Get the important quantities needed to do a retrieval later.
+    
+    Parameters
+    ----------
+    f_org : np.ndarray
+        The original spectra to be approximated.
+    f_err : np.ndarray
+        The error of the spectra.
+    cutoff_index : int
+        The number of wavelength points to use in the basis.
+    use_mean_error : bool
+        Whether to use the mean error in the reconstruction.
+    
+    Returns
+    -------
+    s : set of int
+        The set of bases.
+    coeffs : np.ndarray
+        The basis coefficients.
+    f_rec : np.ndarray
+        The reconstructed observation.
+    """
+    s: Set[int] = search_next_best(f_org, f_err, cutoff_index, use_mean_error)
+    coeffs: np.ndarray = get_coeffs(f_org[:,:cutoff_index], f_err[:,:cutoff_index], s, use_mean_error)
+    f_rec: np.ndarray = get_reconstruction(f_org, coeffs, s)
+    return s, coeffs, f_rec
